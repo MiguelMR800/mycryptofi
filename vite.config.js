53b3e4dd621f722adef1224d66770530347a2b61
Cryptobase <159823915+MiguelMR800@users.noreply.github.com>
2025-10-17 04:50:55 +0300

    Update base path in vite.config.js
    Changed base path for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // tärkeä GitHub Pagesille
+  base: "", // käytä tyhjää, ei './' eikä '/'
 });
